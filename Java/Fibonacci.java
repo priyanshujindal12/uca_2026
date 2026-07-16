2330598b79502622551f4e63093b4a8471b2b42d
priyanshujindal12 <priyanshujindal009@gmail.com>
2026-07-16 21:45:00 +0530

    update

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -1,24 +1,33 @@
-
 import java.util.Scanner;
 
 public class Fibonacci {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        EvenFibonacciNumbers(n);
+
+        System.out.println(sumEvenFibonacci(n));
     }
 
-    static void EvenFibonacciNumbers(int n) {
-        int prev1 = 0;
-        int prev2 = 1;
-        for (int i = 2; i <= n; i++) {
-            int next = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = next;
-            if (i % 3 == 0) {
-                System.out.print(prev2 + " ");
+    static int sumEvenFibonacci(int n) {
+
+        int sum = 0;
+
+        int currentEvenFib = 2;
+        int previousEvenFib = 0;
+        if (n >= 2) {
+            sum += 2;
+        }
+        while (previousEvenFib <= n) {
+            int nextEvenFib = 4 * currentEvenFib + previousEvenFib;
+            previousEvenFib = currentEvenFib;
+            currentEvenFib = nextEvenFib;
+            if (nextEvenFib <= n) {
+                sum += nextEvenFib;
             }
         }
+
+        return sum;
     }
 }
